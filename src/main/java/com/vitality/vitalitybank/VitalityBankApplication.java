60331a92a644e6e036430fc42e0358740f65cdad
Victor Bassey <viktorbasi545@gmail.com>
2020-01-06 19:35:57 +0100

    Initial project setup
    Closes #1

---
+++ src/main/java/com/vitality/vitalitybank/VitalityBankApplication.java
@@ -0,0 +1,13 @@
+package com.vitality.vitalitybank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VitalityBankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VitalityBankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/vitality/vitalitybank/VitalityBankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vitality.vitalitybank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VitalityBankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
